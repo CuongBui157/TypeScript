33c36a01fb5ed135d6fb6d10e115e9af1555cd24
CuongBui157 <cuongbui1572003@gmail.com>
2023-03-25 10:25:09 +0700

    commit1

--- lesson4/src/app.jsx
+++ lesson4/src/app.jsx
@@ -1,16 +1,19 @@
-import Avatar from "./components/avatar"
+import { useState } from "react"
+// import Avatar from "./components/avatar"
 
 // Props: Là một đối tượng, để truyền dữ liệu từ cha xuống con, nhưng không có chiều ngược lại
 // Props: Immutable >< mutable
 const App = () => {
-    const user = {
-        name: "Bui Tien Cuong",
-        role: "Student"
+    const [count, setCount] = useState(10)
+    
+    const increase = () => {
+        setCount(count + 1)
+        console.log(count)
     }
-    return <>
-        <h1>Hello</h1>
-        <Avatar user={user} showInfo={ ()=>console.log(user.name)} />
-    </>
+    return <div className="flex justify-center item-center h-[5vh]">
+        <div>Count: {count}</div>
+        <button onClick={increase} className="bg-red-300">+1 count</button>
+    </div>
 }
 
 export default App
